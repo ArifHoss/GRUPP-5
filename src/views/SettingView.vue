78b47b072a712048c91167c877ca83a8d89077d2
Prakaikool <prakaikool.teepraken@gmail.com>
2025-03-03 04:38:26 +0100

    Add base data for template

--- src/views/SettingView.vue
+++ src/views/SettingView.vue
@@ -1,1 +1,16 @@
+<script setup></script>
+<template>
+  <div class="setting-container">
+    <img src="@/assets/images/happy-earth.png" alt="Happy Planet" class="success-icon" />
 
+    <h1>Förnamn Efternamn</h1>
+    <h2>Email</h2>
+
+    <button @click="Update - password" class="update-password-button">Uppdateara lösenord</button>
+
+    <button @click="Logout" class="Logout-button">Logga ut</button>
+
+    <p>Radera konto</p>
+  </div>
+</template>
+<style scoped></style>
